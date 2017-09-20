5d36cbbff6285bb906a50c55bc57660cc7fa4aea
oliverpople <oliverpople@gmail.com>
2017-09-20 11:50:13 +0100

    Commiting fizzbuzz final

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,16 @@
-require_relative '../lib/fizzbuzz'
+require 'fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'return "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+  end
+  it 'return "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'return "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'return number when passed a number not divisable by 3 or 5' do
+    expect(fizzbuzz(7)).to eq 7
   end
 end
